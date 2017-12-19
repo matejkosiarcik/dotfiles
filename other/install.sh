045004d6b9b2843ec96b8212e63a9554c62d67aa
Matej Košiarčik <matejkosiarcik@gmail.com>
2017-12-19 20:03:58 +0100

    [ADD] Add install script for other scripts

---
+++ other/install.sh
@@ -0,0 +1,12 @@
+# shellcheck shell=sh
+set -euf
+
+[ -d "${HOME}/bin" ] || mkdir "${HOME}/bin"
+cd "$(dirname "${0}")"
+
+install() {
+    cp "${1}.sh" "${HOME}/bin/${1}"
+    chmod +x "${HOME}/bin/${1}"
+}
+
+install 'cldir'
