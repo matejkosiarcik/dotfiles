bcac73fdc49a0668531065b284796c25495dfa18
Matej Košiarčik <matejkosiarcik@gmail.com>
2018-03-20 16:26:52 +0100

    [DOC] Add comment

--- other/cldir.sh
+++ other/cldir.sh
@@ -6,7 +6,7 @@ if [ "${#}" -le '1' ]; then
     exit 1
 fi
 
-dot_clean -m "${1}"
+dot_clean -m "${1}" # for ._* files # BSD
 find "${1}" -name '.DS_Store' -exec rm -f {} \;  # macOS
 find "${1}" -name '.localized' -exec rm -f {} \; # macOS
 find "${1}" -name 'Thumbs.db' -exec rm -f {} \;  # Windows
