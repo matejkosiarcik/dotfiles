7f04ff985f82b6626ac9ced81095f7f39c76c6ba
Matej Košiarčik <matej.kosiarcik@gmail.com>
2019-06-19 09:32:02 +0200

    Move /default-apps to /setup

--- setup/install.sh
+++ setup/install.sh
@@ -5,3 +5,14 @@ cd "$(dirname "${0}")"
 case "$(uname -s)" in
 'Darwin') sh 'macOS.sh' ;;
 esac
+
+# TODO: make it compatible with linux
+if [ "$(uname -s)" == "Darwin" ]; do
+    while IFS="" read -r line; do
+        duti -s 'com.microsoft.VSCode' "${line}" all
+    done <'textfiles.txt'
+
+    while IFS="" read -r line; do
+        duti -s 'org.videolan.vlc' "${line}" all
+    done <'mediafiles.txt'
+fi
