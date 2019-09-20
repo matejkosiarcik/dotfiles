c2a8f130f0077469a25c007901a40ed1dbdbd74c
Matej Košiarčik <matej.kosiarcik@gmail.com>
2019-09-20 13:23:00 +0200

    Fix brew install script

--- dependencies/brew/install.sh
+++ dependencies/brew/install.sh
@@ -2,8 +2,4 @@
 set -euf
 cd "$(dirname "${0}")"
 
-## System-level package managers ##
-if [ "$(uname)" = 'Darwin' ]; then
-    brew bundle
-    find '.' -name 'Brewfile.*' | xargs -n1 cat | brew bundle --file=-
-fi
+brew bundle
