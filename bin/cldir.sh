6e58937c29539670a6461766941fb4a0f2c8aedc
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-01-02 16:23:48 +0100

    feat: remove more files in cldir

--- bin/cldir.sh
+++ bin/cldir.sh
@@ -11,7 +11,22 @@ if [ "${#}" -ge 1 ]; then
     path="${1}"
 fi
 
-dot_clean -m "${path}"                                      # for ._* files # BSD
-find "${path}" -name '.DS_Store' -exec rm -f {} \;          # macOS
+### System Level stuff ###
+
+dot_clean -m "${path}"                                      # for ._* files # macOS, BSD
+find "${path}" -name '.DS_Store' -type f -exec rm -f {} \;  # macOS
 find "${path}" -name '.localized' -type f -exec rm -f {} \; # macOS
-find "${path}" -name 'Thumbs.db' -exec rm -f {} \;          # Windows
+find "${path}" -name 'Thumbs.db' -type f -exec rm -f {} \;  # Windows
+
+### Development build files ###
+
+find "${path}" -name 'build' -type d -exec rm -rf {} \; # General
+find "${path}" -name '.build' -type d -exec rm -rf {} \; # SPM
+find "${path}" -name 'node_modules' -type d -exec rm -rf {} \; # NPM
+find "${path}" -name 'Pods' -type d -exec rm -rf {} \; # CocoaPods
+find "${path}" -name 'Carthage' -type d -exec rm -rf {} \; # Carthage
+find "${path}" -name 'target' -type d -exec rm -rf {} \; # Rust
+find "${path}" -name 'CmakeFiles' -type d -exec rm -rf {} \; # Cmake
+find "${path}" -name 'fastlane/screenshots' -type d -exec rm -rf {} \; # Carthage
+
+# '.build|.git|.venv|*.xcodeproj|bower_components|build|external|Carthage|CMakeFiles|CMakeScripts|node_modules|Pods|target|vendor|venv'
