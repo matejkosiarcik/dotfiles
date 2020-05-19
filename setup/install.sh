679d2ead288f4544a0b4435c05dee90643a49c41
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-05-19 21:59:52 +0200

    feat: add more filetypes for default program association

--- setup/install.sh
+++ setup/install.sh
@@ -6,13 +6,20 @@ case "$(uname -s)" in
 'Darwin') sh 'macOS.sh' ;;
 esac
 
-# TODO: make it compatible with linux
-if [ "$(uname -s)" = "Darwin" ]; then
+# TODO: make it compatible with linux/windows
+make_default_program() {
     while IFS="" read -r line; do
-        duti -s 'com.microsoft.VSCode' "${line}" all
-    done <'textfiles.txt'
+        filetype="$(printf '%s' "${line}" | sed -E 's~#.+~~;s~ ~~g')"
+        if [ "${filetype}" = '' ]; then continue; fi
+        if [ "$(uname -s)" = 'Darwin' ]; then
+            duti -s "${1}" "${filetype}" all
+            associated_app="$(duti -x "${filetype}")"
+            if ! printf '%s' "${associated_app}" | grep "${1}" >/dev/null; then
+                printf 'Filetype %s could not be changed from %s\n' "${filetype}" "$(printf '%s' "${associated_app}" | tr '\n' ' ')"
+            fi
+        fi
+    done <"${2}"
+}
 
-    while IFS="" read -r line; do
-        duti -s 'org.videolan.vlc' "${line}" all
-    done <'mediafiles.txt'
-fi
+make_default_program 'com.microsoft.VSCode' 'textfiles.txt'
+make_default_program 'org.videolan.vlc' 'mediafiles.txt'
