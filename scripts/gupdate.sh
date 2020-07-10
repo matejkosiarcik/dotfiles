0012fc154257eeaa56e5a70ddaba93f2565b1e99
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-07-10 20:27:35 +0200

    feat: add more package-managers to update script

--- scripts/gupdate.sh
+++ scripts/gupdate.sh
@@ -1,25 +1,43 @@
 #!/bin/sh
 set -euf
 
-# macOS
+# macOS system
 if [ "$(uname -s)" = 'Darwin' ]; then
     brew update
     brew upgrade
     brew cask upgrade
     brew cleanup
+
+    softwareupdate --install --all
 fi
 
-# Linux
+# Linux system
 if [ "$(uname -s)" = 'Linux' ]; then
     if command -v apt-get >/dev/null 2>&1; then
-        apt-get update --yes
+        apt-get update
         apt-get upgrade --yes
+        apt-get clean
     fi
+    if command -v apk >/dev/null 2>&1; then
+        apk update
+    fi
+    if command -v yum >/dev/null 2>&1 && ! command -v dnf >/dev/null 2>&1; then
+        yum update
+        yum upgrade
+    fi
+    if command -v dnf >/dev/null 2>&1; then
+        dnf upgrade
+    fi
+fi
+
+if [ "$(uname -s)" = 'Windows' ]; then
+    choco upgrade chocolatey
+    choco upgrade all
 fi
 
 # Python
 pip3 install --upgrade pip setuptools wheel
-pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install --upgrade
+pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install --upgrade
 
 # JavaScript
 npm update -g
