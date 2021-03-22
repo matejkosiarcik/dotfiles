3d0a6bad73aa22ccc8c1fc5bd4cfd2770130ae1e
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-03-22 12:00:41 +0100

    feat: add dir2sha script for external HDD data

---
+++ scripts/dirs2sha.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -euf
+
+if [ "${#}" -lt 1 ]; then
+    printf 'Not enough arguments. Expected dirpath.\n' "${PWD}" >&2
+fi
+root="${1}"
+
+find "${root}" -type d  -mindepth 1 -maxdepth 1 -print0 -not -iname '.git' | while read -r -d $'\0' file; do
+    filename="$(printf '%s\n' "${file}" | sed "s~${root}~~" | tr '/' '|')"
+    printf 'filename: %s\n' "${filename}"
+    # dir2sha "${file}" >"${filename}"
+done
