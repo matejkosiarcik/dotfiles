52de7eed790ec45475b522c993da35a1a7e67519
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-03-22 12:03:11 +0100

    feat: update macOS setup script with new info

--- setup/macOS.sh
+++ setup/macOS.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # This file setups fresh macOS installation
 set -euf
+sudo -v
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
 
 #
 # General system setup
@@ -9,6 +14,9 @@ set -euf
 # show ~/Library
 chflags nohidden "${HOME}/Library"
 
+# show /Volumes
+sudo chflags nohidden /Volumes
+
 # set system to restart when it freezes
 sudo systemsetup -setrestartfreeze on
 
@@ -16,34 +24,70 @@ sudo systemsetup -setrestartfreeze on
 sudo nvram SystemAudioVolume=' '
 
 #
-# Defaults of whole system
+## Global ##
 #
 
-# show filename extensions
-defaults write 'NSGlobalDomain' AppleShowAllExtensions -bool true
+# quit windows when quitting app
+defaults write 'NSGlobalDomain' NSQuitAlwaysKeepsWindows -bool false
 
-# disable this "smart" witchcraft
+# expand save panel by default
+defaults write 'NSGlobalDomain' NSNavPanelExpandedStateForSaveMode -bool true
+defaults write 'NSGlobalDomain' NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# do not create .DS_Store on network volumes
+defaults write 'com.apple.desktopservices' DSDontWriteNetworkStores -bool true
+
+# do not ask to backup into new disk
+defaults write 'com.apple.timemachine' DoNotOfferNewDisksForBackup -bool true
+
+#
+## Keyboard ##
+#
+
+# disable this "smart" quoting witchcraft
 defaults write 'NSGlobalDomain' NSAutomaticDashSubstitutionEnabled -bool false
 defaults write 'NSGlobalDomain' NSAutomaticQuoteSubstitutionEnabled -bool false
 
 # disable autocorrect
 defaults write 'NSGlobalDomain' NSAutomaticSpellingCorrectionEnabled -bool false
 
-# expand save panel by default
-defaults write 'NSGlobalDomain' NSNavPanelExpandedStateForSaveMode -bool true
-
-# quit windows when quitting app
-defaults write 'NSGlobalDomain' NSQuitAlwaysKeepsWindows -bool false
-
 #
-# Defaults of specific apps
+## Dock ##
 #
 
-# do not create .DS_Store on network volumes
-defaults write 'com.apple.desktopservices' DSDontWriteNetworkStores -bool true
+# highlight hover effect
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
 
-# show whitespace in xcode
-defaults write 'com.apple.dt.xcode' DVTTextShowInvisibleCharacters -int 1
+# set icon size for Dock items 36px
+defaults write com.apple.dock tilesize -int 36
+
+# change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "scale"
+
+# minimize windows into their app's icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# enable spring loading
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+
+# show indicator lights for open apps
+defaults write com.apple.dock show-process-indicators -bool true
+
+# wipe all default apps
+defaults write com.apple.dock persistent-apps -array
+
+# always show dock
+defaults write com.apple.dock autohide -bool false
+
+# don't show recent apps in Dock
+defaults write com.apple.dock show-recents -bool false
+
+#
+## Finder ##
+#
+
+# show filename extensions
+defaults write 'NSGlobalDomain' AppleShowAllExtensions -bool true
 
 # show hidden files in finder
 defaults write 'com.apple.finder' AppleShowAllFiles -bool true
@@ -60,6 +104,27 @@ defaults write 'com.apple.finder' _FXShowPosixPathInTitle -bool true
 # disable warning when changing a file extension
 defaults write 'com.apple.finder' FXEnableExtensionChangeWarning -bool false
 
+# sidebar icon size
+defaults write 'NSGlobalDomain' NSTableViewDefaultSizeMode -int 2
+
+#
+## Safari ##
+#
+
+# disable opening 'safe' (ou right) files after download
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
+#
+## Xcode ##
+#
+
+# show whitespace in xcode
+defaults write 'com.apple.dt.xcode' DVTTextShowInvisibleCharacters -int 1
+
+#
+## Unused Apple apps ;) ##
+#
+
 # copy address as 'foo@bar.com' instead of 'Foo <foo@bar.com>'
 defaults write 'com.apple.mail' AddressesIncludeNameOnPasteboard -bool false
 
@@ -69,9 +134,6 @@ defaults write 'com.apple.textedit' PlainTextEncodingForWrite -int 4
 
 # save new files in plain text
 defaults write 'com.apple.textedit' RichText -int 0
-
-# do not ask to backup into new disk
-defaults write 'com.apple.timemachine' DoNotOfferNewDisksForBackup -bool true
 
 #
 # Reload changes
