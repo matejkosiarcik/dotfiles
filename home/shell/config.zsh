59d9c2a2a089a362345a33bd02b6bf10bedd3cee
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-05-23 23:36:31 +0200

    style: fix sticklerci bash errors

--- home/shell/config.zsh
+++ home/shell/config.zsh
@@ -2,7 +2,7 @@
 # shellcheck shell=bash
 
 # shellcheck source=/dev/null
-source "$HOME/.config.sh"
+. "$HOME/.config.sh"
 
 # can not be in ".sh", because builtin is not available in classic sh
 cd() {
