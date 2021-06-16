8d1b88646430c9ddd963fab6fcfb291507a91a84
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-06-16 23:46:16 +0200

    fix: find arguments on linux
    Apparently -mindepth/-maxdepth are global options and should be first, before local options such as -type

--- scripts/dirs2sha.sh
+++ scripts/dirs2sha.sh
@@ -8,7 +8,7 @@ if [ "${#}" -lt 1 ]; then
 fi
 root="${1}"
 
-find "$root" -type d -mindepth 1 -maxdepth 1 -not -iname '.git' | while read -r file; do
+find "$root" -mindepth 1 -maxdepth 1 -type d -not -iname '.git' | while read -r file; do
     filename="$(printf '%s\n' "$file" | sed "s~$root~~" | tr '/' '|')"
     printf 'filename: %s\n' "$filename"
     dir2sha "$file" >"$filename.txt"
