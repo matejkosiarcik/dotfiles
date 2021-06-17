3c856d78b35bb444a0a27331c681d809c2d520f2
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-06-17 13:47:45 +0200

    feat: possibly better unicode handling for dir2sha

--- scripts/dir2sha.sh
+++ scripts/dir2sha.sh
@@ -11,6 +11,7 @@ input="${1}"
 # traverse directory, for each file output it's name and sha1 hash
 # [sorted by filenames]
 find "$input" -type f -print0 |
+    sort --version-sort --ignore-case -s --zero-terminated |
+    xargs -0 -n1 python3 -c 'import unicodedata; import sys; print(unicodedata.normalize("NFC", sys.argv[1]), end="\0")' |
     xargs -0 -n1 shasum --binary |
-    sed -E "s~^([0-9a-f]+) [ *]$input(.+)\$~\1 \2~" |
-    sort --key=2 --version-sort --ignore-case -s
+    sed -E "s~^([0-9a-f]+) [ *]$input(.+)\$~\1 \2~"
