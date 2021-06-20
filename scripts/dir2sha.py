14fde1ffde43cc15b31a8d712bfe9297807508d4
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-06-20 09:40:15 +0200

    feat: replace dir2sha.sh with dir2sha.py
    Should have better/more-consistent cross-platform behaviour and output

--- scripts/dir2sha.py
+++ scripts/dir2sha.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+
 import os
+import re
+import subprocess
 import sys
 import unicodedata
 from typing import List
@@ -8,13 +12,23 @@ from typing import List
 # - normalizes unicode output encoding
 # - sorts files alphabetically (case insensitive)
 def main(args: List[str]):
-    output_files = []
-    for root, _, files in os.walk(args[0], topdown=False):
+    root_dir = os.path.abspath(os.path.realpath(args[0]))
+    print(f"Searching {root_dir}", file=sys.stderr)
+
+    found_files = []
+    for root, _, files in os.walk(root_dir, topdown=False):
         for file in files:
-            output_files.append(unicodedata.normalize("NFC", os.path.join(root, file)))
-    output_files.sort(key=str.casefold)
-    for file in output_files:
-        print(file, end="\0")
+            found_files.append(unicodedata.normalize("NFC", os.path.join(root, file)))
+
+    print(f"Found {len(found_files)} files", file=sys.stderr)
+    found_files.sort(key=str.casefold)
+    print("Sorted files", file=sys.stderr)
+
+    for file in found_files:
+        output = subprocess.check_output(["shasum", "--binary", file], shell=False).decode("utf-8")
+        # output = unicodedata.normalize("NFC", output)
+        output = re.sub(fr"^([0-9a-f]+) [ *]{root_dir}(.+)$", r"\1 \2", output).strip()
+        print(output)
 
 
 if __name__ == "__main__":
