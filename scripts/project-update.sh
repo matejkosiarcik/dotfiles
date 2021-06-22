44e7f75fa807c0bc1cb14786f06b58b2eafbc470
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-06-22 22:31:10 +0200

    feat: add target option to project-update script

--- scripts/project-update.sh
+++ scripts/project-update.sh
@@ -4,6 +4,36 @@ set -euf
 # TODO: dry-run
 # TODO: target version (major, minor, patch)
 
+print_help() {
+    printf 'Usage: project-update [-h] [-t <target>]\n'
+    printf '\n'
+    printf '  -h                       print help message\n'
+    printf '  -t {major, minor patch}  semver upgrade target\n'
+}
+
+target='major'
+while getopts "h?t:" o; do
+    case "${o}" in
+        t)
+            target="${OPTARG}"
+            ;;
+        h)
+            print_help
+            exit 0
+            ;;
+        *)
+            print_help
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$target" != 'major' ] && [ "$target" != 'minor' ] && [ "$target" != 'patch' ]; then
+    printf 'Unsupported target %s\n' "$target"
+    print_help
+    exit 1
+fi
+
 glob() {
     while [ "$#" -ge 1 ]; do
         git ls-files "$1" "*/$1"
@@ -14,32 +44,45 @@ glob() {
 
 # NodeJS
 printf '## NodeJS ##\n'
+ncu_target="$target"
+if [ "$ncu_target" = 'major' ]; then
+    ncu_target='latest'
+fi
 glob 'package.json' | while read -r file; do
     if [ ! -e "$file" ]; then
         continue
     fi
-    ncu --cwd "$(dirname "$file")" --target latest --upgrade # main
+    ncu --cwd "$(dirname "$file")" --target "$ncu_target" --upgrade # main
     (cd "$(dirname "$file")" && relock)                      # lock
 done
 
 # Python
 printf '## Pip ##\n'
+pur_target="--$target"
 glob '*requirements*.txt' | while read -r file; do
     if [ ! -e "$file" ]; then
         continue
     fi
-    pur --force --requirement "$file"
+    if [ "$pur_target" = '--major' ]; then
+        pur --force --requirement "$file"
+    else
+        pur --force "$pur_target" --requirement "$file"
+    fi
 done
 
 # Rust
-printf '## Cargo ##\n'
-glob 'Cargo.toml' | while read -r file; do
-    if [ ! -e "$file" ]; then
-        continue
-    fi
-    (cd "$(dirname "$file")" && cargo upgrade) # main
-    (cd "$(dirname "$file")" && cargo update)  # lock
-done
+if [ "$target" = major ]; then
+    printf '## Cargo ##\n'
+    glob 'Cargo.toml' | while read -r file; do
+        if [ ! -e "$file" ]; then
+            continue
+        fi
+        (cd "$(dirname "$file")" && cargo upgrade) # main
+        (cd "$(dirname "$file")" && cargo update)  # lock
+    done
+else
+    printf '## Skipping cargo ##\n'
+fi
 
 # Gitman
 printf '## Gitman ##\n'
