848b5e1bd671f22202cbb09dd3bf52fc984f16af
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-07-05 19:59:08 +0200

    feat: add checks around packages to backup

--- scripts/backup-packages.sh
+++ scripts/backup-packages.sh
@@ -7,59 +7,69 @@ tmpdir="$(mktemp -d)"
 cd "$tmpdir"
 printf 'Running packages backup...\n'
 
-if [ "$(uname -s)" = 'Darwin' ]; then
-    printf '%s\n' '-- Brew --'
+if command -v brew >/dev/null 2>&1; then
+    printf '# Brew #\n'
     brew bundle dump --file Brewfile
     brew list --formula -1 >brew.txt
     brew list --cask -1 >brew-cask.txt
-elif [ "$(uname -s)" = 'Linux' ]; then
-    if command -v apt >/dev/null 2>&1; then
-        printf '%s\n' '-- Apt --'
-        apt list --installed >apt.txt
-    elif command -v dnf >/dev/null 2>&1; then
-        printf '%s\n' '-- Dnf --'
-        dnf list installed >dnf.txt
-    fi
 fi
 
-printf '%s\n' '-- JavaScript --'
-npm list -g --depth 0 >npm.txt
+if command -v apt >/dev/null 2>&1 && command -v apt-get >/dev/null 2>&1; then
+    printf '# Apt #\n'
+    apt list --installed >apt.txt
+fi
+if command -v dnf >/dev/null 2>&1; then
+    printf '# Dnf #\n'
+    dnf list installed >dnf.txt
+fi
 
-printf '%s\n' '-- Python --'
-pip3 list --format=freeze >requirements.txt
+printf '# JavaScript #\n'
+if command -v npm >/dev/null 2>&1; then
+    npm list -g --depth 0 >npm.txt
+fi
 
-printf '%s\n' '-- Ruby --'
-gem list --quiet >Gemfile
+printf '# Python #\n'
+if command -v pip3 >/dev/null 2>&1; then
+    pip3 list --format=freeze >requirements.txt
+fi
 
-printf '%s\n' '-- Rust --'
-cargo install --list >cargo.txt
-printf '\n\n' >>cargo.txt
-cargo --list >>cargo.txt
+printf '# Ruby #\n'
+if command -v gem >/dev/null 2>&1; then
+    gem list --quiet >Gemfile
+fi
 
-# printf '%s\n' '-- Haskell --'
+printf '# Rust #\n'
+if command -v cargo >/dev/null 2>&1; then
+    cargo install --list >cargo.txt
+    printf '\n\n' >>cargo.txt
+    cargo --list >>cargo.txt
+fi
+
+# printf '# Haskell #\n'
 # cabal list --installed >cabal.txt
 # ghc-pkg list >ghc-pkg.txt
 
-printf '%s\n' '-- Swift --'
-mint list >Mintfile
+printf '# Swift #\n'
+if command -v mint >/dev/null 2>&1; then
+    mint list >Mintfile
+fi
 
+printf '# Apps #\n'
 if [ "$(uname -s)" = 'Darwin' ]; then
-    printf '%s\n' '-- Apps --'
     ls -1 '/Applications' >apps.txt
 fi
 
-printf '%s\n' '-- Editors --'
-code --list-extensions >vscode-extensions.txt
+printf '# Editors #\n'
+if command -v code >/dev/null 2>&1; then
+    code --list-extensions >vscode-extensions.txt
+fi
 case "$(uname -s)" in
 Darwin) cp "$HOME/Library/Application Support/Code/User/settings.json" 'vscode-settings.json' ;;
 Windows) cp %APPDATA%\\Code\\User\\settings.json vscode-settings.json ;;
 Linux) cp "$HOME/.config/Code/User/settings.json" 'vscode-settings.json' ;;
 esac
 
-printf '%s\n' '-- Cron --'
-crontab -l >cron.txt
-
-touch ".$(date '+%Y-%m-%d %H-%M-%S%z')" # mark datetime of current export
+touch ".$(date '+%Y-%m-%dT%H-%M-%S%z')" # mark datetime of current export
 
 # Copy created directory to Dropbox under this computer's name
 computername="$(scutil --get ComputerName || cat /etc/hostname || uname -n)"
