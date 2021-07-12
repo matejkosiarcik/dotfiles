401c4350ee40fc0fd26e2c06bbd60d18eeede8ee
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-07-12 09:03:55 +0200

    feat: update script for file management

--- scripts/dir-clean.sh
+++ scripts/dir-clean.sh
@@ -1,15 +1,82 @@
-#!/bin/sh
+#!/usr/bin/env bashs
 set -euf
 
-# Do not try to pass the directory as an argument
-# The directory must be in cwd
-if [ "$#" -gt 0 ]; then
-    printf 'Too many arguments\n'
+usage() {
+    printf 'Usage: dir-clean dir [-h] [-n] [-i] [-f]\n'
+    printf ' dir    directory to clean (recursively)\n'
+    printf ' -h     show help message\n'
+    printf ' -n     dry run\n'
+    printf ' -i     interactive\n'
+    printf ' -f     force\n'
+}
+
+if [ "$#" -lt 1 ]; then
+    printf 'Not enough arguments\n\n' >&2
+    usage >&2
+    exit 1
 fi
+dir="$1"
+shift
+
+mode=''
+while getopts "h?n?i?f?" opt; do
+    case "$opt" in
+    h)
+        usage
+        ;;
+    n)
+        mode='n'
+        ;;
+    i)
+        mode='i'
+        ;;
+    f)
+        mode='f'
+        ;;
+    *)
+        usage >&2
+        exit 1
+        ;;
+    esac
+done
+if [ "$mode" = '' ]; then
+    printf 'No mode specified (specify either -n|-i|-f)\n\n' >&2
+    usage >&2
+    exit 1
+fi
+printf '%s\n' "$mode"
+
+handle_file() {
+    mode="$1"
+    file="$2"
+
+    case "$mode" in
+    n)
+        printf 'Would remove %s\n' "$file"
+        ;;
+    i)
+        read -r -p "Remove $file? [y/N] " response
+        if [ "$response" = "y" ] || [ "$response" = "Y" ]; then
+            printf 'Removing %s\n' "$file"
+            rm -rf "$file"
+        fi
+        ;;
+    f)
+        printf 'Removing %s\n' "$file"
+        rm -rf "$file"
+        ;;
+    *)
+        printf 'Unrecognized mode: %s\n' "$mode"
+        exit 1
+        ;;
+    esac
+}
+export -f handle_file
 
 printf '### Remove dev folders ###\n'
 find . -type d \( \
     -name '.bundle' -or \
+    -name '.gradle' -or \
     -name '.idea' -or \
     -name '.venv' -or \
     -name '*.framework' -or \
@@ -24,7 +91,7 @@ find . -type d \( \
     -name 'target' -or \
     -name 'vendor' -or \
     -name 'venv' \
-    \) -prune -exec sh -c 'printf "Remove: %s\n" "$0" && rm -rf "$0"' '{}' \;
+    \) -prune -exec sh -c 'handle_file "$0" "$1"' "$mode" '{}' \;
 
 printf '### Remove OS junk files and dev cache files ###\n'
 find . -type f \( \
@@ -38,11 +105,55 @@ find . -type f \( \
     -iname 'ehthumbs.db' -or \
     -iname 'ehthumbs_vista.db' -or \
     -iname 'desktop.ini' \
-    \) -exec sh -c 'printf "Remove: %s\n" "$0" && rm -f "$0"' '{}' \;
+    \) -exec sh -c 'handle_file "$0" "$1"' "$mode" '{}' \;
+
+printf '### Remove Windows reserved files ###\n'
+find . \( -iname 'CON' \
+    -or -iname 'PRN' \
+    -or -iname 'AUX' \
+    -or -iname 'NUL' \
+    -or -iname 'COM1' \
+    -or -iname 'COM2' \
+    -or -iname 'COM3' \
+    -or -iname 'COM4' \
+    -or -iname 'COM5' \
+    -or -iname 'COM6' \
+    -or -iname 'COM7' \
+    -or -iname 'COM8' \
+    -or -iname 'COM9' \
+    -or -iname 'LPT1' \
+    -or -iname 'LPT2' \
+    -or -iname 'LPT3' \
+    -or -iname 'LPT4' \
+    -or -iname 'LPT5' \
+    -or -iname 'LPT6' \
+    -or -iname 'LPT7' \
+    -or -iname 'LPT8' \
+    -or -iname 'LPT9' \
+    \) -exec sh -c 'handle_file "$0" "$1"' "$mode" '{}' \;
 
 # remove symlinks
 # printf '### Remove symlinks ###\n'
 # find . -type l -exec sh -c 'printf "%s\n" "{}"' \;
 
 printf '### Remove extended attributes ###\n'
-xattr -rc .
+case "$mode" in
+n)
+    printf 'Would remove attributes from %s\n' "$dir"
+    ;;
+i)
+    read -r -p "Remove attributes from $dir? [y/N] " response
+    if [ "$response" = "y" ] || [ "$response" = "Y" ]; then
+        printf 'Removing attributes from %s\n' "$dir"
+        xattr -rc "$dir"
+    fi
+    ;;
+f)
+    printf 'Removing attributes from %s\n' "$dir"
+    xattr -rc "$dir"
+    ;;
+*)
+    printf 'Unrecognized mode: %s\n' "$mode"
+    exit 1
+    ;;
+esac
---
+++ scripts/find-bad-files.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -euf
+
+#TODO: search for these chars
+# * . " / \ [ ] : ; | , < > { } '
