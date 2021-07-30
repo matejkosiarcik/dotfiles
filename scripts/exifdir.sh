84fe46ba9918e3a26a7900661c8ae441ca6894ad
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-07-30 17:36:41 +0200

    docs: comment exifdir script

--- scripts/exifdir.sh
+++ scripts/exifdir.sh
@@ -34,23 +34,29 @@ find . \( -iname '*.jpg' -or -iname '*.jpeg' \) -type f | sort --version-sort | 
     filedir="$dir/$(dirname "$file")"
     filename="$(basename "$file")"
 
+    # Get photo creation date
     date="$(exiftool -short -short -short -CreateDate "$file" 2>/dev/null | sed 's~:~-~g;s~\.~-~g;s~ ~_~g')"
     if [ "$date" = '' ]; then
         date="$(exiftool -short -short -short -DateTimeOriginal "$file" 2>/dev/null | sed 's~:~-~g;s~\.~-~g;s~ ~_~g')"
     fi
     if [ "$date" = '' ]; then
+        # "-FileModifyDate" is often not reliable, but better than nothing if the previous methods yield nada
         date="$(exiftool -short -short -short -FileModifyDate "$file" 2>/dev/null | sed 's~:~-~g;s~\.~-~g;s~ ~_~g' | sed -E 's~\+.+$~~')"
     fi
 
     if [ "$date" != '' ]; then
-        printf '%s: %s\n' "$date" "$filename"
+        printf '%s: %s\n' "$date" "$filename" >&2
         if [ "$mode" = 'f' ]; then
             newfilename="$date.jpg"
+
+            # Check if the new file already exists (2 photos with the exact date taken)
+            # In such case name the second photo with an iterator suffix
             i=0
             while [ -e "$filedir/$newfilename" ]; do
                 i="$((i + 1))"
                 newfilename="$date $i.jpg"
             done
+
             mv "$filedir/$filename" "$filedir/$newfilename"
         fi
     else
@@ -58,6 +64,9 @@ find . \( -iname '*.jpg' -or -iname '*.jpeg' \) -type f | sort --version-sort | 
     fi
 done
 
+# If there were some collisions (multiple photos with the same exact date)
+# The first photo will lack suffix (as a consequence, it will be sorted last in programs such as file explorer)
+# So add 0 suffix to it
 if [ "$mode" = 'f' ]; then
     find . -name '*-*-* *-*-* 1.jpg' -type f | sort --version-sort | while read -r file; do
         filedate="$(basename "$file" ' 1.jpg')"
@@ -66,7 +75,7 @@ if [ "$mode" = 'f' ]; then
         if [ ! -e "$newfile" ]; then
             mv "$oldfile" "$newfile"
         else
-            printf '%s already exist!\n' "$newfile"
+            printf '%s already exist!\n' "$newfile" >&2
         fi
     done
 fi
