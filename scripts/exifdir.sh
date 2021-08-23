a1a02df78f976bec8d847133e4ada2647b03794c
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-08-23 17:44:52 +0200

    feat: add more filetypes to exifdir

--- scripts/exifdir.sh
+++ scripts/exifdir.sh
@@ -30,20 +30,31 @@ fi
 dir="$2"
 cd "$dir"
 
-find . \( -iname '*.jpg' -or -iname '*.jpeg' \) -type f | sort --version-sort | while read -r file; do
+find . \( \
+    -iname '*.3gp' -or \
+    -iname '*.avi' -or \
+    -iname '*.jpeg' -or \
+    -iname '*.jpg' -or \
+    -iname '*.m4a' -or \
+    -iname '*.m4v' -or \
+    -iname '*.mov' -or \
+    -iname '*.mp4' -or \
+    -iname '*.wav' \
+\) -type f | sort --version-sort | while read -r file; do
     filedir="$dir/$(dirname "$file")"
     filename="$(basename "$file")"
+    fileext="$(printf '%s' "$filename" | sed -E 's~^.*\.~~')"
 
     # Get photo creation date
     date="$(exiftool -short -short -short -CreateDate "$file" 2>/dev/null | sed 's~:~-~g;s~\.~-~g;s~ ~_~g')"
-    if [ "$date" = '' ]; then
+    if [ "$date" = '' ] || [ "$date" = '0000-00-00_00-00-00' ] || [ "$date" = '0000-00-00' ]; then
         date="$(exiftool -short -short -short -DateTimeOriginal "$file" 2>/dev/null | sed 's~:~-~g;s~\.~-~g;s~ ~_~g')"
     fi
-    if [ "$date" = '' ]; then
-        # "-FileModifyDate" is often not reliable, but better than nothing if the previous methods yield nada
+    if [ "$date" = '' ] || [ "$date" = '0000-00-00_00-00-00' ] || [ "$date" = '0000-00-00' ]; then
+        # "-FileModifyDate" is often not reliable, but better than nothing if the previous methods yield nothing
         date="$(exiftool -short -short -short -FileModifyDate "$file" 2>/dev/null | sed 's~:~-~g;s~\.~-~g;s~ ~_~g' | sed -E 's~\+.+$~~')"
     fi
-    if [ "$date" = '0000-00-00_00-00-00' ]; then
+    if [ "$date" = '0000-00-00_00-00-00' ] || [ "$date" = '0000-00-00' ]; then
         # unset date when it is bogus
         date=''
     fi
@@ -51,14 +62,14 @@ find . \( -iname '*.jpg' -or -iname '*.jpeg' \) -type f | sort --version-sort | 
     if [ "$date" != '' ]; then
         printf '%s: %s\n' "$date" "$filename" >&2
         if [ "$mode" = 'f' ]; then
-            newfilename="$date.jpg"
+            newfilename="$date.$fileext"
 
             # Check if the new file already exists (2 photos with the exact date taken)
             # In such case name the second photo with an iterator suffix
             i=0
             while [ -e "$filedir/$newfilename" ]; do
                 i="$((i + 1))"
-                newfilename="$date $i.jpg"
+                newfilename="$date $i.$fileext"
             done
 
             mv "$filedir/$filename" "$filedir/$newfilename"
@@ -72,10 +83,22 @@ done
 # The first photo will lack suffix (as a consequence, it will be sorted last in programs such as file explorer)
 # So add 0 suffix to it
 if [ "$mode" = 'f' ]; then
-    find . -name '*-*-* *-*-* 1.jpg' -type f | sort --version-sort | while read -r file; do
-        filedate="$(basename "$file" ' 1.jpg')"
-        oldfile="$(dirname "$file")/$filedate.jpg"
-        newfile="$(dirname "$file")/$filedate 0.jpg"
+    find . \( \
+    -iname '*.3gp' -or \
+    -iname '*.avi' -or \
+    -iname '*.jpeg' -or \
+    -iname '*.jpg' -or \
+    -iname '*.m4a' -or \
+    -iname '*.m4v' -or \
+    -iname '*.mov' -or \
+    -iname '*.mp4' -or \
+    -iname '*.wav' \
+    \) -and -name "* 1.*" -type f | sort --version-sort | while read -r file; do
+        filename="$(basename "$file")"
+        fileext="$(printf '%s' "$filename" | sed -E 's~^.*\.~~')"
+        filedate="$(basename "$file" " 1.$fileext")"
+        oldfile="$(dirname "$file")/$filename"
+        newfile="$(dirname "$file")/$filedate 0.$fileext"
         if [ ! -e "$newfile" ]; then
             mv "$oldfile" "$newfile"
         else
