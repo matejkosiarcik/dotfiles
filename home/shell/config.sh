7378036a9c00653692a68249e60bc91f4ea52e10
Matej Košiarčik <matej.kosiarcik@gmail.com>
2022-12-11 21:34:30 +0100

    feat: just a bunch of changes to scripts

--- home/shell/config.sh
+++ home/shell/config.sh
@@ -10,12 +10,14 @@ export GPG_TTY
 # Aliases
 alias tree='tree --ignore-case -CI ".build|.git|.hg|.svn|.venv|*.xcodeproj|*.xcworkspace|bower_components|build|external|Carthage|CMakeFiles|CMakeScripts|node_modules|Pods|target|vendor|venv"'
 alias pbfix='pbpaste | pbcopy'
+alias o='open'
 alias exa='exa --long --tree --git-ignore --time modified --time-style long-iso --extended  --all --ignore-glob ".git"'
 alias diff='git diff --no-index'
 
 # Docker aliases
-alias dr='docker run --interactive --tty --rm'
 alias db='docker build . --rm  --tag'
+alias dr='docker run --interactive --tty --rm'
+alias dbentry='docker run --interactive --tty --rm --entrypoint=bash'
 
 # Git aliases
 alias df='git df | h'
@@ -30,7 +32,7 @@ alias g='git'
 alias ghpr='gh pr create --assignee matejkosiarcik --title "Automated PR" --body "" && sleep 2 && gh pr merge --auto --merge'
 
 # New Gitlab MR to myself and automatically merge it
-alias glmr='glab mr create --assignee matej.kosiarcik --title "Automated MR" --description "" --remove-source-branch && sleep 2 && glab mr merge "$(git branch --show-current)" --when-pipeline-succeeds --remove-source-branch'
+# alias glmr='glab mr create --assignee matej.kosiarcik --title "Automated MR" --description "" --remove-source-branch && sleep 2 && glab mr merge "$(git branch --show-current)" --when-pipeline-succeeds --remove-source-branch'
 
 # Download video/audio from youtube with best quality
 # `-f best` is not enough, because it is limited to 1080p (IIRC)
@@ -38,7 +40,7 @@ alias ytdv='youtube-dl --ignore-error --format "bestvideo[ext=mp4]+bestaudio[ext
 alias ytda='youtube-dl --ignore-error --format "bestaudio[ext=m4a]" --'
 
 # download from uloz.to using ulozto-downloader wrapped in docker
-alias uloztod='docker run --interactive --tty --rm --volume "${PWD}:/downloads" matejkosiarcik/ulozto-downloader:dev --output /downloads --auto-captcha --parts 10'
+# alias uloztod='docker run --interactive --tty --rm --volume "${PWD}:/downloads" matejkosiarcik/ulozto-downloader:dev --output /downloads --auto-captcha --parts 10'
 
 # azlint
 alias azlint='docker run --volume "$PWD:/project:ro" matejkosiarcik/azlint:dev lint'
@@ -47,32 +49,8 @@ alias azc='azfmt --only-changed && azlint --only-changed'
 alias az='azfmt && azlint'
 
 # Other
-alias whatsmyip='curl ipinfo.io'
-
-# Update local repository
-gitup() {
-    if [ "$(git status --short)" != '' ]; then
-        printf 'You have uncommitted changes!\n'
-        return 1
-    fi
-
-    git fetch --all --tags --prune --prune-tags
-    current_branch="$(git rev-parse --abbrev-ref HEAD)"
-    default_branch="$(git remote show origin | grep 'HEAD branch:' | sed -E 's~.+:[ \t]*~~')"
-
-    if [ "$current_branch" = "$default_branch" ]; then
-        # update master/main when it's checked
-        git pull --ff-only || return 1
-    else
-        # update master/main when we are on different branch
-        git fetch origin "$default_branch:$default_branch" || return 1
-    fi
-
-    git branch --merged "$default_branch" | grep -ve '\*' -e "$default_branch" | xargs -n1 git branch -d || return 1
-    if [ "$current_branch" != "$default_branch" ]; then
-        git rebase "$default_branch" || return 1
-    fi
-}
+alias whatsmyip='curl --silent ipinfo.io | jq -r .ip'
+alias csv2json='mlr --icsv --ojson --jlistwrap cat'
 
 # Open new terminal at current directory
 tdup() {
@@ -101,37 +79,19 @@ mcd() {
     cd "${1}" || return 1
 }
 
-mchanges() {
-    find . -mindepth 1 -maxdepth 1 -type d -print0 |
-        xargs -0 -n1 sh -c 'cd "$0" && printf "%s\n" "$(basename $0)" && git status --short'
-}
+# mchanges() {
+#     find . -mindepth 1 -maxdepth 1 -type d -print0 |
+#         xargs -0 -n1 sh -c 'cd "$0" && printf "%s\n" "$(basename $0)" && git status --short'
+# }
 
 # Normalize 'open' on all systems
 if [ "$(uname)" != 'Darwin' ]; then
     if grep -q Microsoft /proc/version; then # Ubuntu on Windows using the Linux subsystem
         alias open='explorer.exe'
     else
-        alias open='xdg-open'
+        alias open='xdg-open' # Linux
     fi
 fi
-
-# Smart alias for 'open'
-o() {
-    if [ "$#" -eq 0 ]; then
-        open .
-    else
-        open "$@"
-    fi
-}
-
-# Smart alias for `code`
-c() {
-    if [ "$#" -eq 0 ]; then
-        code .
-    else
-        code "$@"
-    fi
-}
 
 if [ "$(uname)" != 'Darwin' ]; then
     if command -v xclip >/dev/null 2>&1; then
---
+++ scripts/gitmon.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -euf
+# Monitors files in current directory and calls `command` on change
+# Ignores changes to gitignored files
+
+if [ "$#" -lt 1 ]; then
+    printf 'Command missing\n' >&2
+    exit 1
+fi
+command="$1"
+
+chokidar '**/*' --initial --silent \
+    --command "if ! git check-ignore '{path}' >/dev/null 2>&1; then clear; $command; fi" \
+    --ignore "**/.DS_Store" \
+    --ignore "**/.bundle" \
+    --ignore "**/.idea" \
+    --ignore "**/.pytest_cache" \
+    --ignore "**/.venv" \
+    --ignore "**/.vs" \
+    --ignore "**/.vscode" \
+    --ignore "**/Desktop.ini" \
+    --ignore "**/Thumbs.db" \
+    --ignore "**/__pycache__" \
+    --ignore "**/dist" \
+    --ignore "**/node_modules" \
+    --ignore "**/public" \
+    --ignore "**/target" \
+    --ignore "**/venv"
