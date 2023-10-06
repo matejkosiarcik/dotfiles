96e2a4f5fdea1084d0b6dda91a868a262a93b81e
Matej Košiarčik <matej.kosiarcik@gmail.com>
2023-10-06 22:00:52 +0200

    feat: enable notes attachment import deamon

--- deamons/main.sh
+++ deamons/main.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 mkdir -p "$HOME/.log" "$HOME/Pictures/Import - Raw"
 
 PATH="$PATH:/opt/homebrew/bin"
-# nohup sh notes-attachments-import/main.sh 2>&1 | ts '%Y-%m-%d %H:%M:%.S |' >>"$HOME/.log/notes-attachment-import.txt" &
+nohup sh notes-attachments-import/main.sh 2>&1 | ts '%Y-%m-%d %H:%M:%.S |' >>"$HOME/.log/notes-attachment-import.txt" &
 nohup sh photo-import/main.sh 2>&1 | ts '%Y-%m-%d %H:%M:%.S |' >>"$HOME/.log/photo-import.txt" &
 nohup sh screenrecording-rename/main.sh 2>&1 | ts '%Y-%m-%d %H:%M:%.S |' >>"$HOME/.log/screenrecording-rename.txt" &
 nohup sh screenshots-rename/main.sh 2>&1 | ts '%Y-%m-%d %H:%M:%.S |' >>"$HOME/.log/screenshots-rename.txt" &
