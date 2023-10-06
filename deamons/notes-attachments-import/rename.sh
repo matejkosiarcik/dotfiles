d9a1f6124cce3fb9f8b2a05f9fd8b3b6d60a25f3
Matej Košiarčik <matej.kosiarcik@gmail.com>
2023-10-06 22:00:16 +0200

    feat: handle multiple filetypes when importing attachments for notes

--- deamons/notes-attachments-import/rename.sh
+++ deamons/notes-attachments-import/rename.sh
@@ -20,39 +20,29 @@ if printf '%s' "$old_filename" | grep -E '^\.' >/dev/null; then
     exit 0
 fi
 
-old_extension="$(printf '%s' "$old_filename" | sed -E 's~^.*\.~~')"
-new_extension="$(printf '%s' "$old_extension" | tr '[:upper:]' '[:lower:]')"
-date="$(date +"%Y-%m-%d_%H-%M-%S")"
-new_filename="$date.$new_extension"
-
-# Exit on files not in proper format
-if printf '%s\n' "$old_filename" | grep -E '^[0-9_\-]+( [0-9]+)?\.[a-z0-9]+$' >/dev/null; then
+# Exit on files already in proper format
+if printf '%s\n' "$old_filename" | grep -E '^[0-9]{4}\-[0-9]{2}-[0-9]{2}_[0-9]+\.[a-zA-Z0-9]+$' >/dev/null; then
     exit 0
 fi
 
-# Check if the new file already exists (2 photos with the exact date taken)
-i=1
-if [ -e "$new_filename" ] || [ -e "$date 1.$new_extension" ] || [ -e "$date 01.$new_extension" ] || [ -e "$date 001.$new_extension" ]; then
-    # If we are adding "A.jpg", but it already exists, we will add "A 2.jpg"
-    # But we need to rename the former image to "A 1.jpg"
-    if [ -e "$new_filename" ]; then
-        if [ ! -e "$date 1.$new_extension" ] && [ ! -e "$date 01.$new_extension" ] && [ ! -e "$date 001.$new_extension" ]; then
-            mv "$new_filename" "$date 1.$new_extension"
-        fi
-    fi
-    i=2
-    new_filename="$date $i.$new_extension"
-fi
+old_extension="$(printf '%s' "$old_filename" | sed -E 's~^.*\.~~')"
+new_extension="$(printf '%s' "$old_extension" | tr '[:upper:]' '[:lower:]')"
+date="$(date +"%Y-%m-%d")"
 
-# Determine full filename (increment index until it doesn't exists)
-while [ -e "$new_filename" ]; do
-    if [ "$new_filename" = "$old_filename" ]; then
-        break
-    fi
+# Check if the new file already exists (2 or more attachments imported on the same day)
+i=1
+i_padded="00$i"
+new_filename="${date}_${i_padded}.$new_extension"
+while find . -type f -name "$date\_$i_padded.*" | grep . >/dev/null; do
     i="$((i + 1))"
-    new_filename="$date $i.$new_extension"
+    if [ "$i" -ge 100 ]; then
+        i_padded="$i"
+    elif [ "$i" -ge 10 ]; then
+        i_padded="0$i"
+    else
+        i_padded="00$i"
+    fi
+    new_filename="${date}_${i_padded}.$new_extension"
 done
 
-if [ "$new_filename" != "$old_filename" ]; then
-    mv "$old_filename" "$new_filename"
-fi
+mv "$old_filename" "$new_filename"
