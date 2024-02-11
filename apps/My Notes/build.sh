899055da1f169b39cee1c882afbbb8c5a9d12531
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-02-11 17:05:58 +0100

    build: fix build script (unassigned CI variable)

--- apps/My Notes/build.sh
+++ apps/My Notes/build.sh
@@ -3,6 +3,10 @@ set -euf
 
 cd "$(dirname "$0")"
 
+if [ -z "${CI+x}" ]; then
+    CI=0
+fi
+
 # TODO: Reenable platypus after https://github.com/sveinbjornt/Platypus/issues/262
 if [ "$(uname -s)" != 'Darwin' ] || [ "$CI" = 1 ]; then
     printf 'App build skipped on non-macOS systems' >&2
