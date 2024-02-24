e042d8e27e3330a18f90ec599838d0ce58fe5a18
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-02-24 10:19:23 +0100

    chore: minor print changes

--- deamons/screenrecording-rename/rename.sh
+++ deamons/screenrecording-rename/rename.sh
@@ -18,7 +18,7 @@ old_filename="$(basename "$file")"
 
 # Exit on files not in proper format
 if printf '%s\n' "$old_filename" | grep -vE '^.+ [0-9\-]+ at [0-9\.]+\.[a-zA-Z0-9]+$' >/dev/null; then
-    printf 'Skip %s\n' "$old_filename"
+    printf 'Skipping (wrong filename format) %s\n' "$old_filename"
     exit 0
 fi
 
