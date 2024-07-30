a06dd889608ccbe67322959cdc908c1f3d7993a2
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-30 20:46:02 +0200

    chore: update screenrecording-rename script

--- deamons/screenrecording-rename/rename.sh
+++ deamons/screenrecording-rename/rename.sh
@@ -7,24 +7,30 @@ if [ "$#" -lt 1 ]; then
     exit 1
 fi
 
-file="$1"
-if [ ! -e "$file" ]; then
-    printf 'Skipping file (not found) - %s\n' "$(basename "$file")"
-    exit 0
-fi
-
-filename="$(basename "$file")"
-
-# Exit on files not in proper format
-if printf '%s\n' "$filename" | grep -vE '^.+ [0-9\-]+ at [0-9\.]+\.[a-zA-Z0-9]+$' >/dev/null; then
-    printf 'Skipping file (wrong filename format) - %s\n' "$filename"
-    exit 0
-fi
-if printf '%s\n' "$filename" | grep -vE '^Screenshot ' >/dev/null; then
-    printf 'Skipping file (filename prefix mismatch) - %s\n' "$filename"
+old_filepath="$1"
+if [ ! -e "$old_filepath" ]; then
+    # printf 'Skipping file (not found) - %s\n' "$(basename "$file")"
     exit 0
 fi
 
 targetdir="$HOME/Movies/Screenrecording"
+if [ ! -d "$targetdir" ]; then
+    mkdir -p "$targetdir"
+fi
 
-mv "$file" "$targetdir/$filename"
+old_filename="$(basename "$old_filepath")"
+
+# Exit on files not in proper format
+if printf '%s\n' "$old_filename" | grep -v -E '/Screenshot [0-9]{4}-[0-9]{2}-[0-9]{2} at [0-9]{2}\.[0-9]{2}\.[0-9]{2}\.mov$' >/dev/null; then
+    # printf 'Skipping file (wrong filename format) %s\n' "$old_filename"
+    exit 0
+fi
+
+date="$(printf '%s\n' "$(basename "$file")" | sed -E 's~^.+ ([0-9]{4})-([0-9]{2})-([0-9]{2}) at ([0-9]{2})\.([0-9]{2})\.([0-9]{2})\.[a-zA-Z0-9]+$~\1-\2-\3_\4-\5-\6~')"
+extension="$(printf '%s\n' "$(basename "$file")" | sed -E 's~^.+\.([a-zA-Z0-9]+)$~\1~' | tr '[:upper:]' '[:lower:]')"
+new_filename="$date.$extension"
+new_filepath="$targetdir/new_filename"
+
+if [ "$new_filename" != "$old_filename" ]; then
+    mv "$old_filepath" "$new_filepath"
+fi
