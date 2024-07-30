5b507fe433762ce7ad5b586490885221790eb66f
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-30 21:13:24 +0200

    chore: fix shellcheck errors

--- deamons/photo-import/main.sh
+++ deamons/photo-import/main.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -euf
 
-# shellcheck disable=SC2016
-# ^^^ Disabled for fswatch | xargs
-
 PATH="/opt/homebrew/bin:$HOME/.matejkosiarcik-dotfiles/bin:$PATH"
 export PATH
 
@@ -21,9 +18,11 @@ find "$watchdir" \
     \( -iname '*.jpe' -or -iname '*.jpg' -or -iname '*.jpeg' -or -iname '*.png' -or -iname '*.mov' -or -iname '*.mp4' -or -name '*.heic' -or -name '*.heif' \) |
     grep -E -i '\.(?:hei[cf]|jpeg|jp[eg]|mov|mp4|png)$' |
     grep -E -v '/[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}(?: [0-9]+)?\.[A-Za-z0-9-]+$' >"$tmpfile" || true
+# shellcheck disable=SC2016
 xargs -n1 sh -c 'photo-exif-rename "$1" || true' - <"$tmpfile"
 rm -f "$tmpfile"
 
 # Watch for new files
+# shellcheck disable=SC2016
 fswatch "$watchdir" --event=Created --event=MovedTo --event=Renamed -E --exclude '(^|/)\..+$' --exclude '(^|/)[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}( [0-9]+)?\.[A-Za-z0-9-]+$' --print0 |
     xargs -0 -n1 sh -c 'if [ -f "$1" ]; then photo-exif-rename "$1" || true; fi' -
--- deamons/screenshots-rename/main.sh
+++ deamons/screenshots-rename/main.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -euf
 
-# shellcheck disable=SC2016
-# ^^^ Disabled for fswatch | xargs
-
 source_dir="$(dirname "$(readlink "$0")")"
 export source_dir
 
@@ -20,9 +17,11 @@ fi
 tmpfile="$(mktemp)"
 find "$watchdir" -maxdepth 1 -type f -iname '*.png' -print0 |
     grep -E -i -z '/Screenshot [0-9]{4}-[0-9]{2}-[0-9]{2} at [0-9]{2}\.[0-9]{2}\.[0-9]{2}(?: [0-9]+)?\.png$' >"$tmpfile" || true
+# shellcheck disable=SC2016
 xargs -0 -n1 sh -c 'sh "$source_dir/rename.sh" "$1" || true' - <"$tmpfile"
 rm -f "$tmpfile"
 
 # Watch for new files
+# shellcheck disable=SC2016
 fswatch "$watchdir" --event=Created --event=MovedTo --event=Renamed -E --exclude '(^|/)\..+$' --exclude '(^|/)[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}( [0-9]+)?\.png$' --print0 |
     xargs -0 -n1 sh -c 'if [ -f "$2" ]; then sh "$1/rename.sh" "$2" || true; fi' - "$source_dir"
