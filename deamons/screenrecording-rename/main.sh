6134f122d7d9318381222fb6c65ce5c60ae2514f
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-30 20:43:27 +0200

    chore: update screenrecording-rename script

--- deamons/screenrecording-rename/main.sh
+++ deamons/screenrecording-rename/main.sh
@@ -1,30 +1,25 @@
 #!/bin/sh
 set -euf
 
-# Disable for xargs
-# shellcheck disable=SC2016
-
 source_dir="$(dirname "$(readlink "$0")")"
 export source_dir
 
-PATH="$source_dir/python/bin:/opt/homebrew/bin:$PATH"
+PATH="/opt/homebrew/bin:$PATH"
 export PATH
-PYTHONPATH="$source_dir/python"
-export PYTHONPATH
 
+# Prepare target directory
 watchdir="$HOME/Desktop"
+if [ ! -d "$watchdir" ]; then
+    mkdir -p "$watchdir"
+fi
 
-# Move existing files
+# Rename existing files
 tmpfile="$(mktemp)"
-find "$watchdir" -maxdepth 1 -type f \( -iname '*.mov' -or -iname '*.mp4' \) -print0 >"$tmpfile"
-xargs -0 -n1 sh "$source_dir/move.sh" <"$tmpfile"
+find "$watchdir" -maxdepth 1 -type f -iname '*.png' -print0 |
+    grep -E -i -z '/Screenshot [0-9]{4}-[0-9]{2}-[0-9]{2} at [0-9]{2}\.[0-9]{2}\.[0-9]{2}\.mov$' >"$tmpfile" || true
+xargs -0 -n1 sh -c 'sh "$source_dir/rename.sh" "$1" || true' - <"$tmpfile"
 rm -f "$tmpfile"
 
-# Watch for changes and rename newly added files
-watchmedo shell-command "$watchdir" \
-    --wait \
-    --quiet \
-    --ignore-directories \
-    --patterns '*.mov;*.mp4' \
-    --command 'if [ "$watch_object" = file ] && { [ "$watch_event_type" = created ] || [ "$watch_event_type" = modified ]; }; then sleep 1 && sh "$source_dir/move.sh" "$watch_src_path"; fi'
-# NOTE: We could also listen for "move" events, but it would be really easy to fall into infinite loop
+# Watch for new files
+fswatch "$watchdir" --event=Created --event=MovedTo --event=Renamed -E --exclude '(^|/)\..+$' --print0 |
+    xargs -0 -n1 sh -c 'if [ -f "$2" ]; then sh "$1/rename.sh" "$2" || true; fi' - "$source_dir" # shellcheck disable=SC2016
