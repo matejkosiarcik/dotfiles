2b3639dbbadd83c0e024643066083273c69bff35
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-03-17 21:39:56 +0100

    fix: photo import sphandling of psaces for existing files

--- deamons/photo-import/main.sh
+++ deamons/photo-import/main.sh
@@ -15,11 +15,12 @@ tmpfile="$(mktemp)"
 find "$watchdir" \
     -maxdepth 1 \
     -type f \
-    \( -iname '*.jpe' -or -iname '*.jpg' -or -iname '*.jpeg' -or -iname '*.png' -or -iname '*.mov' -or -iname '*.mp4' -or -name '*.heic' -or -name '*.heif' \) |
-    grep -E -i '\.(?:hei[cf]|jpeg|jp[eg]|mov|mp4|png)$' |
-    grep -E -v '/[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}(?: [0-9]+)?\.[A-Za-z0-9-]+$' >"$tmpfile" || true
+    -print0 \
+    \( -iname '*.jpe' -or -iname '*.jpg' -or -iname '*.jpeg' -or -iname '*.png' -or -iname '*.mov' -or -iname '*.mp4' -or -name '*.heic' -or -name '*.heif' \)|
+    grep -E -i -z '\.(?:hei[cf]|jpeg|jp[eg]|mov|mp4|png)$' |
+    grep -E -v -z '/[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}(?: [0-9]+)?\.[A-Za-z0-9-]+$' >"$tmpfile" || true
 # shellcheck disable=SC2016
-xargs -n1 sh -c 'photo-exif-rename "$1" || true' - <"$tmpfile"
+xargs -0 -n1 sh -c 'photo-exif-rename "$1" || true' - <"$tmpfile"
 rm -f "$tmpfile"
 
 # Watch for new files
