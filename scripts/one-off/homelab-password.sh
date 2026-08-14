6998f93f746056a4caa176fdfed8338a46f2cde1
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-08-14 16:42:51 +0200

    feat: add one-off script for generating homelab passwords

---
+++ scripts/one-off/homelab-password.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -euf
+
+count='1'
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+    -c | --count)
+        count="$2"
+        shift 2
+        ;;
+    *)
+        printf 'Unknown argument %s\n' "$1"
+        exit 1
+        ;;
+    esac
+done
+
+i='0'
+while [ "$i" -lt "$count" ]; do
+    part_1="$(bw generate --lowercase --uppercase --number --length 15)"
+    part_2="$(openssl rand -hex 5)" # alphanumeric length is 2x
+    part_3="$(bw generate --lowercase --uppercase --number --length 15)"
+    printf '%s%s%s\n' "$part_1" "$part_2" "$part_3"
+    i="$((i+1))"
+done
